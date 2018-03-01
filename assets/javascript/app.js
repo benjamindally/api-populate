81ad29e2753cf25148727196c6355453fb22057b
Benjamin <benjamin.dally@icloud.com>
2018-02-28 22:15:43 -0800

    Mostly Done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,76 @@
-$(document).ready(function(){
-    var borderButtons = ["Charlie Chaplin", "Buster Keaton", "Greta Garbo", "Harold Lloyd", "Max Linder", "Douglas Fairbanks" , "Lillian Gish"];
-    var par = $("<p>");
-    var div = $("<div>");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?apikey=KEisq4X4yK4PbMGynfJOqzvFkjVTYKtY&q="
+$(document).ready(function() {
+  var borderButtons = [
+    "Charlie Chaplin",
+    "Buster Keaton",
+    "Greta Garbo",
+    "Max Linder",
+    "Douglas Fairbanks",
+    "Lillian Gish",
+  ];
+  var par = $("<p>");
+  var div = $("<div>");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?apikey=KEisq4X4yK4PbMGynfJOqzvFkjVTYKtY&q=";
+  var buttonText = "test";
 
-    for (i = 0; i < borderButtons.length; i++){
-        var buttonDisplay = $(".button_bar");
-        var btn = $("<button>");
-        btn.append(borderButtons[i]);
-        btn.attr("button-name", borderButtons[i]);
-        buttonDisplay.append(btn);
+  $(".add_button").on("click", function() {
+    var userInput = $(".user_field_form")
+      .val()
+      .trim();
+    borderButtons.push(userInput);
+    createButtons();
+    console.log(borderButtons);
+    $(".user_field_form").val("");
+  });
 
+  createButtons();
+
+  function createButtons() {
+    $(".button_bar").empty();
+
+    for (i = 0; i < borderButtons.length; i++) {
+      var buttonDisplay = $(".button_bar");
+      var btn = $("<button>");
+      btn.addClass("star_button");
+      btn.append(borderButtons[i]);
+      btn.attr("button-name", borderButtons[i]);
+      buttonDisplay.append(btn);
     }
+    getGifs();
+  }
 
-    $("button").on("click", function(){
-        var buttonText = $(this).attr("button-name");
-        $.ajax({
+  function getGifs() {
+    $(".star_button").on("click", function() {
+      buttonText = $(this).attr("button-name");
+      console.log(buttonText);
+      $.ajax({
         url: queryURL + buttonText,
-        method: "GET"
+        method: "GET",
       }).then(function(response) {
         var results = response.data;
-        console.log(response);   
+        console.log(response);
 
         for (i = 0; i < 10; i++) {
-            if (results.rating != "r"){
-                var gifDiv = $(`<div class="image_divs">`)
-                var imageUrl = results[i].images.fixed_height.url;
-                var rating = results[i].rating;
-                var rating2 = rating.toUpperCase();
-                var title = results[i].title;
-                var image = $("<img>");
-                var p = $("<p>").text("Rating: " + rating2);
-                var p2 = $("<p>").text("Rating: " + title);
+          if (results[i].rating !== "r") {
+            var gifDiv = $(`<div class="image_divs">`);
+            var imageUrl = results[i].images.fixed_height_still.url;
+            var gifUrl = results[i].images.fixed_height.url;
+            var rating = results[i].rating;
+            var rating2 = rating.toUpperCase();
+            var title = results[i].title;
+            var image = $("<img>");
+            var p = $("<p>").text("Rating: " + rating2);
 
-                image.attr("src", imageUrl);
-                image.attr("alt", buttonText + " Image");
+            image.attr("src", imageUrl);
+            image.attr("alt", buttonText + " Image");
 
-                gifDiv.append(image);
-                gifDiv.append(p);
-                gifDiv.append(p2);
+            gifDiv.append(image);
+            gifDiv.append(p);
 
-                $(".display_area").prepend(gifDiv);
-            }
+            $(".display_area").prepend(gifDiv);
+          }
         }
-
       });
-    })
-})
+    });
+  }
+});
